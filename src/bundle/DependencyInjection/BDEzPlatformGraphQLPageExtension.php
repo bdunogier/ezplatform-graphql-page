7c3817bdc51a01389d22b0aff5f9a439d93a7b83
Bertrand Dunogier <bertrand.dunogier@ez.no>
2018-10-30 11:29:47 +0100

    Fixed container extension name for good

--- src/bundle/DependencyInjection/BDEzPlatformGraphQLPageExtension.php
+++ src/bundle/DependencyInjection/BDEzPlatformGraphQLPageExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class BDPlatformGraphQLPageExtension extends Extension
+class BDEzPlatformGraphQLPageExtension extends Extension
 {
     /**
      * {@inheritdoc}
