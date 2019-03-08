cfde6a271687186a4b57ee80585d1430776c8d61
Bertrand Dunogier <bertrand.dunogier@ez.no>
2019-03-08 12:08:18 +0100

    Adapted to ezplatform-graphql@d73f5a42 changes
    Uses the path constants from EzSystemsEzPlatformGraphQLBundle instead of extension.

--- src/DependencyInjection/Compiler/PageResolverBlocksTypesPass.php
+++ src/DependencyInjection/Compiler/PageResolverBlocksTypesPass.php
@@ -3,6 +3,7 @@ namespace BD\EzPlatformGraphQLPage\DependencyInjection\Compiler;
 
 use BD\EzPlatformGraphQLPage\GraphQL\PageResolver;
 use EzSystems\EzPlatformGraphQL\DependencyInjection\EzSystemsEzPlatformGraphQLExtension;
+use EzSystems\EzPlatformGraphQL\EzSystemsEzPlatformGraphQLBundle;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Yaml\Yaml;
@@ -14,18 +15,28 @@ class PageResolverBlocksTypesPass implements CompilerPassInterface
 {
     const RESOLVER_ID = PageResolver::class;
 
+    /**
+     * Path of the file where the PageBlocksList type is defined.
+     * @var string
+     */
+    private $pageBlocksIndexFile;
+
+    public function __construct($schemaDir)
+    {
+        $this->pageBlocksIndexFile = $schemaDir . '/PageBlocksList.types.yml';
+    }
+
     public function process(ContainerBuilder $container)
     {
         if (!$container->has(self::RESOLVER_ID)) {
             return;
         }
 
-        $pageBlocksIndexFile = EzSystemsEzPlatformGraphQLExtension::SCHEMA_DIR . '/PageBlocksList.types.yml';
-        if (!file_exists($pageBlocksIndexFile)) {
+        if (!file_exists($this->pageBlocksIndexFile)) {
             return;
         }
 
-        $typesMap = $this->getTypesMap($pageBlocksIndexFile);
+        $typesMap = $this->getTypesMap($this->pageBlocksIndexFile);
         
         $resolverDefinition = $container->getDefinition(self::RESOLVER_ID);
         $resolverDefinition->setArgument('$blocksTypesMap', $typesMap);
